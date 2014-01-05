e59d0b7ea251e18337951ec87031d7fe767adf23
Florian MOREL <m.morel.florian@gmail.com>
2014-01-05 20:14:30 +0100

    Update Parser.hs

--- Parser.hs
+++ Parser.hs
@@ -76,144 +76,145 @@ module Parser(
 			TokEnd ->  (EmptyNode, accept toks)
 
 	direction (token1:toks1) = 
+	
 			case token1 of 
 
-				(TokDir x) ->  let (token2, toks2) = (lookAhead toks1, accept toks1)
-					       in
-							case token2 of
+				(TokDir x) -> let (token2, toks2) = (lookAhead toks1, accept toks1)
+					       			in
+												case token2 of
 
-								(TokDir y) -> case x of 
+												(TokDir y) -> case x of 
 
-									-- Forward can lead to 2 motions, dragonF and half circle B
-									F -> case y of
+																				-- Forward can lead to 2 motions, dragonF and half circle B
+																				F -> case y of
 
-										F -> let (nextNode, restToks) = input toks2
-										     in
-										 	(CommandNode DashF nextNode, restToks)
+																								F -> let (nextNode, restToks) = input toks2
+										     														 in
+										 																		(CommandNode DashF nextNode, restToks)
 
-										D -> dragonF toks2
+																								D -> dragonF toks2
 
-										DF -> halfCB toks2
+																								DF -> halfCB toks2
 
-										otherwise -> let (nextNode, restToks) = input toks2
-										 	     in
-										 		(DirectionNode x (DirectionNode y nextNode), restToks)
+																								otherwise -> let (nextNode, restToks) = input toks2
+																														 in
+										 																						(DirectionNode x (DirectionNode y nextNode), restToks)
 
-									-- Down leads to Quarter Circle F and B
-									D ->  case y of
+																				-- Down leads to Quarter Circle F and B
+																				D ->  case y of
 
-										 DF -> quarterCF toks2
+										 														DF -> quarterCF toks2
 
-										 DB -> quarterCB toks2
+																			 					DB -> quarterCB toks2
 
-										 otherwise -> let (nextNode, restToks) = input toks2
-											      in
-												 (DirectionNode x (DirectionNode y nextNode), restToks)
+																								otherwise -> let (nextNode, restToks) = input toks2
+																														 in
+																																(DirectionNode x (DirectionNode y nextNode), restToks)
 
-									B -> case y of
+																				B -> case y of
 
-										B -> let (nextNode, restToks) = input toks2
-										     in
-										 	(CommandNode DashB nextNode, restToks)																
+																								B -> let (nextNode, restToks) = input toks2
+																										 in
+										 																		(CommandNode DashB nextNode, restToks)																
 
-										D -> dragonB toks2
+																								D -> dragonB toks2
 
-										DB -> halfCF toks2
+																								DB -> halfCF toks2
 
-										otherwise -> let (nextNode, restToks) = input toks2
-										 	     in 
-										 		(DirectionNode x (DirectionNode y nextNode), toks2)
+																								otherwise -> let (nextNode, restToks) = input toks2
+																										 				 in 
+																																(DirectionNode x (DirectionNode y nextNode), toks2)
 
-									otherwise ->  let (nextNode, restToks) = input toks1
-										      in
-											(DirectionNode x nextNode, restToks)
+																				otherwise -> let (nextNode, restToks) = input toks1
+										      					 								 in
+																												(DirectionNode x nextNode, restToks)
 
-								(TokKey k) -> (DirectionNode x (KeyNode k), toks2)
+												(TokKey k) -> (DirectionNode x (KeyNode k), toks2)
 
-  								TokEnd -> (DirectionNode x EmptyNode, []) 
+  											TokEnd -> (DirectionNode x EmptyNode, []) 
 
 
 				otherwise -> error "No direction, in direction"
 
 	halfCF (token1:toks1) = 
 		case token1 of 
-			(TokDir x) -> let (token2, toks2) = (lookAhead toks1, accept toks1) 
-				      in 
-					case x of
+				(TokDir x) -> let (token2, toks2) = (lookAhead toks1, accept toks1) 
+				      				in 
+												case x of
 
-						D -> case token2 of 
+														D -> case token2 of 
 
-							(TokDir y) -> case y of 
+																		(TokDir y) -> case y of 
 
-									DF -> let (token3, toks3) = (lookAhead toks2, accept toks2)
-									      in 
-											case token3 of 
+																										DF -> let (token3, toks3) = (lookAhead toks2, accept toks2)
+									      																	in 
+																															case token3 of 
 
-												(TokDir z) -> let (node, nextToks) = input toks3 
-												      	  	  in
-																case z of
+																																	(TokDir z) -> let (node, nextToks) = input toks3 
+												      	  	  																					in
+																																										case z of
 
-																	F -> (CommandNode HCF node, nextToks)
+																																												F -> (CommandNode HCF node, nextToks)
 
-																	otherwise -> (DirectionNode x (DirectionNode y (DirectionNode z node)), nextToks)
+																																												otherwise -> (DirectionNode x (DirectionNode y (DirectionNode z node)), nextToks)
 
-											(TokKey k) -> (DirectionNode x (DirectionNode y (KeyNode k)), toks3)
+																																	(TokKey k) -> (DirectionNode x (DirectionNode y (KeyNode k)), toks3)
 
-											TokEnd	   -> (DirectionNode x (DirectionNode y EmptyNode), [])
+																																	TokEnd	   -> (DirectionNode x (DirectionNode y EmptyNode), [])
 
-									otherwise -> let (node, nextToks) = input toks2
-										      	 in
-													(DirectionNode x (DirectionNode y node), nextToks)
+																										otherwise -> let (node, nextToks) = input toks2
+										      	 																		 in
+																																		(DirectionNode x (DirectionNode y node), nextToks)
 
-							(TokKey k) -> (DirectionNode x (KeyNode k), toks2)
+																		(TokKey k) -> (DirectionNode x (KeyNode k), toks2)
 
-							TokEnd	   -> (DirectionNode x EmptyNode, [])
+																		TokEnd	   -> (DirectionNode x EmptyNode, [])
 
-						otherwise -> let (node, nextToks) = input toks2
-							     	 in
-										(DirectionNode x node, nextToks)
+														otherwise -> let (node, nextToks) = input toks2
+							     	 										 in
+																						(DirectionNode x node, nextToks)
 
-			(TokKey k) -> (KeyNode k, toks1)
+				(TokKey k) -> (KeyNode k, toks1)
 
-			TokEnd	   -> (EmptyNode, [])
+				TokEnd	   -> (EmptyNode, [])
 
 	halfCB (token1:toks1) = 
 		case token1 of 
-			(TokDir x) -> let (token2, toks2) = (lookAhead toks1, accept toks1) 
-				      	  in 
-							case x of
+				(TokDir x) -> let (token2, toks2) = (lookAhead toks1, accept toks1) 
+				      	  		in 
+												case x of
 
-								D -> case token2 of 
+														D -> case token2 of 
 
-										(TokDir y) -> case y of 
+																		(TokDir y) -> case y of 
 
-														DB -> let (token3, toks3) = (lookAhead toks2, accept toks2)
-									    					  in 
-																case token3 of 
+																											DB -> let (token3, toks3) = (lookAhead toks2, accept toks2)
+									    					  													in 
+																																case token3 of 
 
-																	(TokDir z) -> let (node, nextToks) = input toks3 
-												    							  in
-																					case z of
+																																		(TokDir z) -> let (node, nextToks) = input toks3 
+												    							  																			in
+																																											case z of
 
-																						B -> (CommandNode HCB node, nextToks)
+																																													B -> (CommandNode HCB node, nextToks)
 
-																						otherwise -> (DirectionNode x (DirectionNode y (DirectionNode z node)), nextToks)
+																																													otherwise -> (DirectionNode x (DirectionNode y (DirectionNode z node)), nextToks)
 
-																	(TokKey k) -> (DirectionNode x (DirectionNode y (KeyNode k)), toks3)
+																																		(TokKey k) -> (DirectionNode x (DirectionNode y (KeyNode k)), toks3)
 
-																	TokEnd	   -> (DirectionNode x (DirectionNode y EmptyNode), [])
+																																		TokEnd	   -> (DirectionNode x (DirectionNode y EmptyNode), [])
 
-														otherwise ->  let (node, nextToks) = input toks2
-										      						  in
-																		(DirectionNode x (DirectionNode y node), nextToks)
+																											otherwise ->  let (node, nextToks) = input toks2
+										      						  														in
+																																				(DirectionNode x (DirectionNode y node), nextToks)
 
-										(TokKey k) -> (DirectionNode x (KeyNode k), toks2)
+																		(TokKey k) -> (DirectionNode x (KeyNode k), toks2)
 
-										TokEnd	   -> (DirectionNode x EmptyNode, [])
+																		TokEnd	   -> (DirectionNode x EmptyNode, [])
 
-								otherwise -> let (node, nextToks) = input toks2
-							     			 in
-											 	(DirectionNode x node, nextToks)
+														otherwise -> let (node, nextToks) = input toks2
+							     											 in
+											 											(DirectionNode x node, nextToks)
 
 			(TokKey k) -> (KeyNode k, toks1)
 
@@ -221,60 +222,60 @@ module Parser(
 
 	quarterCF (token:toks) = 
 
-		case token of 
-			(TokDir x) -> let (node, nextToks) = input toks
-						  in
-							case x of
-								F -> (CommandNode QCF node, nextToks)
+			case token of 
+					(TokDir x) -> let (node, nextToks) = input toks
+						  					in
+														case x of
+															F -> (CommandNode QCF node, nextToks)
 
-								otherwise -> (DirectionNode x node, nextToks)
+															otherwise -> (DirectionNode x node, nextToks)
 
-			(TokKey k) -> (KeyNode k, toks)
+					(TokKey k) -> (KeyNode k, toks)
 
-			TokEnd	   -> (EmptyNode, [])
+					TokEnd	   -> (EmptyNode, [])
 
 	quarterCB (token:toks) = 
 
-		case token of
-			(TokDir x) -> 	let (node, nextToks) = input toks
-							in
-								case x of
+			case token of
+					(TokDir x) ->	let (node, nextToks) = input toks
+												in
+														case x of
 
-									B -> (CommandNode QCB node, nextToks)
+																B -> (CommandNode QCB node, nextToks)
 
-									otherwise -> (DirectionNode x node, nextToks)
+																otherwise -> (DirectionNode x node, nextToks)
 
-			(TokKey k) -> (KeyNode k, toks)
+					(TokKey k) -> (KeyNode k, toks)
 
-			TokEnd	   -> (EmptyNode, [])
+					TokEnd	   -> (EmptyNode, [])
 
 	dragonF (token:toks) =
-		case token of
-			(TokDir x) -> 	let (node, nextToks) = input toks
-							in
-								case x of
+			case token of
+					(TokDir x) -> let (node, nextToks) = input toks
+												in
+														case x of
 
-									DF -> (CommandNode DPF node, nextToks)
+																DF -> (CommandNode DPF node, nextToks)
 
-									otherwise -> (DirectionNode x node, nextToks)
+																otherwise -> (DirectionNode x node, nextToks)
 
-			(TokKey k) -> (KeyNode k, toks)
+					(TokKey k) -> (KeyNode k, toks)
 
-			TokEnd	   -> (EmptyNode, [])
+					TokEnd	   -> (EmptyNode, [])
 
 	dragonB (token:toks) = 
-		case token of
-			(TokDir x) -> 	let (node, nextToks) = input toks
-							in
-								case x of
+			case token of
+					(TokDir x) -> let (node, nextToks) = input toks
+												in
+														case x of
 
-									DB -> (CommandNode DPB node, nextToks)
+																DB -> (CommandNode DPB node, nextToks)
 
-									otherwise -> (DirectionNode x node, nextToks)
+																otherwise -> (DirectionNode x node, nextToks)
 
-			(TokKey k) -> (KeyNode k, toks)
+					(TokKey k) -> (KeyNode k, toks)
 
-			TokEnd	   -> (EmptyNode, [])
+					TokEnd	   -> (EmptyNode, [])
 
 	parse :: [Token] -> [Expression]
 	parse [] = []
